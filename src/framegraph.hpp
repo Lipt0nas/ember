5393250e8246b3f52bee59f75c9aa6fcdb58a4e1
Martynas Gurskas <gurmart@gmail.com>
2026-01-24 10:31:52 +0200

    Change framegraph pass and barrier logs to debug
    Signed-off-by: Martynas Gurskas <gurmart@gmail.com>

--- src/framegraph.hpp
+++ src/framegraph.hpp
@@ -608,10 +608,10 @@ struct Framegraph {
         for (int i = 0; i < passes.size(); i++) {
             auto& pass = passes[i];
 
-            spdlog::info("---- Pass {} transitions:", pass.name);
+            spdlog::debug("---- Pass {} transitions:", pass.name);
             for (int b = 0; b < image_barriers[i].size(); b++) {
                 auto& barrier = image_barriers[i][b];
-                spdlog::info(
+                spdlog::debug(
                     "\tTransition image 0x{:x}: {} -> {}",
                     (uintptr_t)barrier.image,
                     string_VkImageLayout(barrier.old_layout),
@@ -620,7 +620,7 @@ struct Framegraph {
             }
             for (int b = 0; b < buffer_barriers[i].size(); b++) {
                 auto& barrier = buffer_barriers[i][b];
-                spdlog::info(
+                spdlog::debug(
                     "\tBuffer barrier 0x{:x}: ({} | {}) -> ({} | {})",
                     (uintptr_t)barrier.buffer,
                     string_VkAccessFlags2(barrier.src_access_mask),
@@ -630,20 +630,20 @@ struct Framegraph {
                 );
             }
 
-            spdlog::info("---- ");
+            spdlog::debug("---- ");
         }
 
-        spdlog::info("---- Post frame transitions:");
+        spdlog::debug("---- Post frame transitions:");
         for (int b = 0; b < post_frame_image_barriers.size(); b++) {
             auto& barrier = post_frame_image_barriers[b];
-            spdlog::info(
+            spdlog::debug(
                 "\tTransition image 0x{:x}: {} -> {}",
                 (uintptr_t)barrier.image,
                 string_VkImageLayout(barrier.old_layout),
                 string_VkImageLayout(barrier.new_layout)
             );
         }
-        spdlog::info("---- ");
+        spdlog::debug("---- ");
     }
 
     void execute(VkCommandBuffer command_buffer, uint32_t frame_index) {
